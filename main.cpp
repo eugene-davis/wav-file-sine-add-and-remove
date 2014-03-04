119fdaeded5ea72f5b9f9f8321b6add72c7dc376
Eugene Davis <eugene_davis@industrialdragonfly.com>
2014-03-04 02:21:40 -0600

    First commit

---
+++ main.cpp
@@ -0,0 +1,21 @@
+/* 
+ * File:   main.cpp
+ * Author: eugene
+ *
+ * Created on March 4, 2014, 1:44 AM
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int
+main(int argc, char** argv)
+{
+
+    return 0;
+}
+
