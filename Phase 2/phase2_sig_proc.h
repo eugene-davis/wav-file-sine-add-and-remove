5207327ec287eafdfee32616269bcce1a8aef835
Eugene Davis <eugene@eugenemdavis.net>
2014-04-13 18:29:58 -0500

    Put comments in phase2_sign_proc.h

--- Phase 2/phase2_sig_proc.h
+++ Phase 2/phase2_sig_proc.h
@@ -1,16 +1,11 @@
 /* 
- * File:   phase1_sig_proc.h
  * Author: Eugene Davis
  * Class: CPE 381
- * CPE 381 Project Phase 1
+ * CPE 381 Project Phase 2
  * 
- * Contains functions specific to Phase 1 of the project other than the main
- * function, specifically functions to find the maximum amplitude of all the
- * channels in a given sample and functions to add a sine wave to the original
- * signal. Both functions include an 8 bit and a 16 bit version.
- * 
- * Created on March 4, 2014, 11:09 PM
- */
+ * Contains functions specific to Phase 2 of the project other than the main
+ * function.
+*/
 
 #ifndef PHASE1_SIG_PROC_H
 #define	PHASE1_SIG_PROC_H
@@ -19,6 +14,10 @@
 extern "C" {
 #endif
 
+	/**
+	* filterSample the buffer of input samples (bufferSample), the coeffecient array and the length of the window
+	* and returns the filtered sample via currentSample
+	*/
 	void filterSample(short* currentSample, short* bufferSample, double* coArray, int length);
 
 #ifdef	__cplusplus
