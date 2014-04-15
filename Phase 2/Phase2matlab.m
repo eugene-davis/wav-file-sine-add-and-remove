e5b3448629bec1b66b1451b71f77766a1b518a3f
Eugene Davis <eugene@eugenemdavis.net>
2014-04-15 00:49:04 -0500

    Got screenshots and script for Matlab and worked more on report

---
+++ Phase 2/Phase2matlab.m
@@ -0,0 +1,11 @@
+%Import noisy file
+[noise, Fs] = wavread('Davis_E_noise.WAV');
+sptool
+% sptool and filter tool used to devlop filter graphically
+
+%Import filtered files
+[lp, Fs] = wavread('Davis_E_lp.WAV');
+[bs, Fs] = wavread('Davis_E_bs.WAV');
+
+sptool
+% Sptool used to find spectrums
